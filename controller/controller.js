b4af0957524729b913dc6a5c47f88f2c5cc413ce
CamilosolerB <caansobu2@gmail.com>
2021-10-19 15:53:29 -0500

    v 1.2
    Funcionamiento de servidor,
    renderizado de la primera vista de usuario

---
+++ controller/controller.js
@@ -0,0 +1,10 @@
+//const conexion = require('../conexion/conexion')
+//const cnn = conexion();
+const bcrypjs = require('bcryptjs')
+const controller  = {};
+
+controller.index=(req,res,next)=>{
+    res.render('login')
+}
+
+module.exports=controller;
--- path/rutas.js
+++ path/rutas.js
@@ -1,1 +1,10 @@
-//rutas
+//llamado de las librerias
+const express = require('express');
+const controller = require('../controller/controller')
+const rutas = express.Router()
+
+//links
+rutas.get('/',controller.index)
+
+
+module.exports=rutas;
