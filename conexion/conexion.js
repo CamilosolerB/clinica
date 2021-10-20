87f8bd2cdf70c5db7b1c27ec49a2cc883bf239e9
Diegoq28 <quevedod67@gmail.com>
2021-10-20 12:03:20 -0500

    subir

--- conexion/conexion.js
+++
@@ -1,9 +0,0 @@
-const mysql =  require('mysql')
-module.exports=()=>
-mysql.createConnection({
-    host:'bgiebq8wmwx2ho0v9gpb-mysql.services.clever-cloud.com',
-    user:'ujpuwetrdavzgjdn',
-    password:'PMmPRJrfXjJXRl0Q8LGT',
-    port:'3306',
-    database:'bgiebq8wmwx2ho0v9gpb'
-})
