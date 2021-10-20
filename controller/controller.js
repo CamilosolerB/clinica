dc3783ad92ea0fc3d860daada68e53c7af8e71cf
CamilosolerB <caansobu2@gmail.com>
2021-10-19 20:42:59 -0500

    consulta

--- controller/controller.js
+++ controller/controller.js
@@ -10,5 +10,18 @@ controller.index=(req,res,next)=>{
 controller.login=(req,res,next)=>{
     res.render('login')
 }
+controller.ingreso=(req,res,next)=>{
+    const nom=req.body.username;
+    const cla=req.body.password;
+
+    cnn.query('SELECT * FROM usuario INNER JOIN doctor ON (doc_usu=documento) WHERE doc_usu=? AND password=?',[nom,cla],(err,resbb)=>{
+        if(err){
+            next(new Error(err))
+        }
+        else{
+            res.render("doctor",{datos:resbb})
+        }
+    })
+}
 
 module.exports=controller;
