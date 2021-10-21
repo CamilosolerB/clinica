1c81238396b60e9660df4e197dcb9a21a9e21e73
CamilosolerB <caansobu2@gmail.com>
2021-10-21 13:46:41 -0500

    algo sencillo

--- controller/controller.js
+++ controller/controller.js
@@ -41,38 +41,79 @@ controller.ingreso=(req,res,next)=>{
     })
 }
 controller.firma=async(req,res,next)=>{
-    /*const doctor = req.body.documentodoc;
-    //const paci = req.body.documentopac;
-    const histo = req.body.historia;
-    const archivo = req.file;
-    console.log(archivo)
-    cnn.query('UPDATE doctor SET firma="'+archivo+'" WHERE documento="'+doctor+'"',(err,resbb)=>{
-        if(err){
-            next(new Error(err))
-        }
-        else{
-            console.log('añadido')
-            res.redirect('/')
-        }
-    })
-    //cnn.query('INSERT INTO historia_clinica SET?',{Id_doctor:doctor,Id_paciente:paci,imagen:histo})
-    //res.send("El archivo se subio")*/
     console.log(req.files)
 
-    const image = ["./public/upload/nombre.PNG","./public/upload/documento.PNG","./public/upload/direccion.PNG","./public/upload/fecha.PNG","./public/upload/informacion.PNG"];
+    const nom="./public/upload/nombre.PNG"
+    const dir="./public/upload/direccion.PNG";
+    const fec ="./public/upload/fecha.PNG"
+    const info = "./public/upload/informacion.PNG";
     
     console.log("Antes del ocr")
     //var nombre,doc;
 
-        tesseract.recognize(image, options)
-        .then((nombre) => {
-            console.log(nombre)
-            res.send(nombre)
+        tesseract.recognize(fec, options)
+        .then((dia) => {
+            const doc=req.body.documentodoc
+            const paci=req.body.documentopac
+            const fech=dia
+            cnn.query('INSERT INTO historia_clinica SET?',{Id_doctor:doc,id_paciente:paci,fecha:fech},async(err,resbb)=>{
+                if(err){
+                    next(new Error(err))
+                }
+                else{
+                    tesseract.recognize(nom, options)
+                    .then((nombr) => {
+                        const doc=req.body.documentodoc
+                        const paci=req.body.documentopac
+                        cnn.query('UPDATE historia_clinica SET nombre="'+nombr+'" WHERE Id_doctor="'+doc+'" AND id_paciente="'+paci+'"',async(err,resbb)=>{
+                            if(err){
+                                next(new Error(err))
+                            }
+                            else{
+                                tesseract.recognize(dir, options)
+                                .then((direct) => {
+                                    const doc=req.body.documentodoc
+                                    const paci=req.body.documentopac
+                                    cnn.query('UPDATE historia_clinica SET direccion="'+direct+'" WHERE Id_doctor="'+doc+'" AND id_paciente="'+paci+'"',async(err,resbb)=>{
+                                        if(err){
+                                            next(new Error(err))
+                                        }
+                                        else{
+                                            tesseract.recognize(info, options)
+                                            .then((clinic) => {
+                                                const doc=req.body.documentodoc
+                                                const paci=req.body.documentopac
+                                                cnn.query('UPDATE historia_clinica SET historia="'+clinic+'" WHERE Id_doctor="'+doc+'" AND id_paciente="'+paci+'"',async(err,resbb)=>{
+                                                    if(err){
+                                                        next(new Error(err))
+                                                    }
+                                                    else{
+                                                        const doc=req.body.documentodoc
+                                                        const paci=req.body.documentopac
+                                                        cnn.query("SELECT * FROM historia_clinica WHERE Id_doctor=? AND id_paciente=?",[doc,paci],(err,resbb)=>{
+                                                            if(err){
+                                                                next(new Error(err))
+                                                            }
+                                                            else{
+                                                                res.render('pdf',{datos:resbb})
+                                                            }
+                                                        })
+                                                        
+                                                    }
+                                        })
+                                    })
+                                }
+                            })
+                        })
+                    }
+                })
             })
+            }
+        })
+    })
             .catch((error)=>{
                 res.send(error.message)
             })
-
 
 
     
